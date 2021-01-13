7e2f29748f1b069f26607647c9bf6149026e6c3b
adler-sudo <52939328+adler-sudo@users.noreply.github.com>
2021-01-12 18:16:23 -0800

    added comments

--- CovertingCoordinates/TestSets/input_data_sample.py
+++ CovertingCoordinates/TestSets/input_data_sample.py
@@ -14,7 +14,10 @@ def subsample(file='../Data/Complete_ASOtoTranscriptSeq.tsv'):
         100-row randomly sampled pandas dataframe
     """
 
+    # read in the tsv
     df = pd.read_csv(file, sep=' ')
+
+    # randomly subsample 100 rows
     subsample_df = df.sample(n=100)
 
     return subsample_df
