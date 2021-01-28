43f53574b9345abbb354123a03b84323de192497
adler-sudo <52939328+adler-sudo@users.noreply.github.com>
2021-01-27 19:42:38 -0800

    adding checks for non-matching asos and genes

---
+++ unique_checks/gene_match_check.py
@@ -0,0 +1,28 @@
+# checks for genes that don't have a single aso match
+
+# import necessary modules
+import pandas as pd
+from Bio.Seq import Seq
+
+
+# read in the whole dataframe
+df = pd.read_csv("../Data/Complete_ASOtoTranscriptSeq.tsv", sep=" ")
+
+# list unique geneIDs
+unique_genes = df.GeneID.unique()
+
+# determine number of times each gene has aso in sequence
+gene_check = {}
+for i in unique_genes:
+    gene_check[i] = 0
+
+for i, r in df.iterrows():
+    if str(Seq(r.ASOseq).reverse_complement()) in r.Sequence:
+        gene_check[str(r.GeneID)] = gene_check[str(r.GeneID)] + 1
+
+# identify genes with no matching ASOs to sequence
+missing_genes = []
+for i in gene_check.keys():
+    if gene_check[i] == 0:
+        missing_genes.append(i)
+
