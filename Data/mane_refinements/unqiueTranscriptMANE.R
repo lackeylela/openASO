f60f293617e63742f1e545e4807a20761251e8f5
adler-sudo <jamespadler@gmail.com>
2021-04-17 12:42:14 -0700

    unique_id script

--- Data/mane_refinements/unqiueTranscriptMANE.R
+++ Data/mane_refinements/unqiueTranscriptMANE.R
@@ -3,7 +3,7 @@
 
 # read in the 2 csvs
 mane_tx_id = read.csv('geneToTranscript.csv') # MANE table
-data = read.csv('Data/Complete_ASOtoTranscriptSeq.tsv',
+data = read.csv('../Complete_ASOtoTranscriptSeq.tsv',
                 sep='',stringsAsFactors = FALSE) # primary project dataset
 
 # generate new column for eventual unique tx id
@@ -19,5 +19,20 @@ for (gene in unique_genes){
   }, error = function(e){})
 }
 
+# substring transcript name to get rid of .[0-9]
+data$hg38.knownGene.name <- substr(data$hg38.knownGene.name, 0, 15)
+data$tx_id <- substr(data$tx_id, 0, 15)
+
+# create unique ids
+data$unique_id <- paste(data$GeneID, " _ ", data$ASOseq)
+
+# drop transcripts that don't match the mane transcripts for each gene
+mane_refine = data[which(data$hg38.knownGene.name == data$tx_id),]
+
+# drop all duplicates (ie PIK3CD was dropped completely)
+mane_refine = mane_refine[which(table(mane_refine$unique_id) == 1),]
+
 # write table to tsv
-write.table(data, file='completeASO_w_maneRefine.tsv', sep='\t', col.names=NA)
+write.table(mane_refine, file='C:/Users/james/R/openASO/openASOcorrect/Data/uniqueIds.tsv',
+            sep='\t',
+            row.names=FALSE)
